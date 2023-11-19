8c6a7c4c5852f6e731d9197f26da642dd753c5ca
KOLPARK <127416437+KOLPARK@users.noreply.github.com>
2023-11-19 23:03:50 +0900

    Add files via upload

---
+++ splash_screen.dart
@@ -0,0 +1,84 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/common/const/colors.dart';
+import 'package:flutter_auth/common/const/data.dart';
+import 'package:flutter_auth/common/layout/default_layout.dart';
+import 'package:flutter_auth/common/view/root_tab.dart';
+import 'package:flutter_auth/user/view/login_screen.dart';
+import 'package:gap/gap.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    deleteToken();
+    checkToken();
+  }
+
+  void deleteToken() async {
+    await storage.deleteAll();
+  }
+
+  void checkToken() async {
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+    final dio = Dio();
+
+    try {
+      final response = await dio.post(
+        'http://$ip/auth/token',
+        options: Options(
+          headers: {
+            'authorization': 'Bearer $refreshToken',
+          },
+        ),
+      );
+      await storage.write(
+        key: ACCESS_TOKEN_KEY,
+        value: response.data['accessToken'],
+      );
+
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(
+          builder: (_) => const RootTab(),
+        ),
+        (route) => false,
+      );
+    } catch (e) {
+
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(
+          builder: (_) => const LoginScreen(),
+        ),
+        (route) => false,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      backgroundColor: PRIMARY_COLOR,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Gap(16),
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
